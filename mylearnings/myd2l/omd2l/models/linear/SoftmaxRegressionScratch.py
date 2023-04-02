384834acf726a004baa6d6547441f41c50590142
omroot <oualid.missaoui@gmail.com>
2023-04-02 13:13:44 -0400

    softmax regression implementation

--- mylearnings/myd2l/omd2l/models/linear/SoftmaxRegressionScratch.py
+++ mylearnings/myd2l/omd2l/models/linear/SoftmaxRegressionScratch.py
@@ -1,4 +1,6 @@
 
+
+
 import numpy as np
 import torch
 import torchvision
@@ -40,20 +42,34 @@ from torch import nn
 from torch.nn import functional as F
 from torchvision import transforms
 
+
 from omd2l.models.base.Classifier import Classifier
 
-class SoftmaxRegression(Classifier):
-    """Defined in :numref:`sec_softmax_concise`"""
-    def __init__(self, num_outputs, lr):
+class SoftmaxRegressionScratch(Classifier):
+    def __init__(self, num_inputs, num_outputs, lr, sigma=0.01):
         super().__init__()
         self.save_hyperparameters()
-        self.net = nn.Sequential(nn.Flatten(),
-                                 nn.LazyLinear(num_outputs))
-    def forward(self, X):
-        return self.net(X)
+        self.W = torch.normal(0, sigma, size=(num_inputs, num_outputs),
+                              requires_grad=True)
+        self.b = torch.zeros(num_outputs, requires_grad=True)
 
-    def loss(self, Y_hat, Y, averaged=True):
-        Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
-        Y = Y.reshape((-1,))
-        return F.cross_entropy(
-            Y_hat, Y, reduction='mean' if averaged else 'none')
+    def parameters(self):
+        return [self.W, self.b]
+
+    def softmax(self, X):
+        X_exp = torch.exp(X)
+        partition = X_exp.sum(1, keepdims=True)
+        return X_exp / partition  # The broadcasting mechanism is applied here
+
+    def cross_entropy(self, y_hat, y):
+        return - torch.log(y_hat[list(range(len(y_hat))), y]).mean()
+
+
+    def loss(self, y_hat, y):
+        return self.cross_entropy(y_hat, y)
+
+    def forward(self, X):
+        return self.softmax(torch.matmul(X.reshape((-1, self.W.shape[0])), self.W) + self.b)
+
+
+
